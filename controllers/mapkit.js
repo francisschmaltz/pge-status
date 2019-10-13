f6913b272f3835ce7f0c330eb13a9d38e99ef818
Francis Schmaltz <francis@yaboiii.com>
2019-10-12 23:33:24 -0700

    remove uneeded dependencies and added origin protection for mapkit key

--- controllers/mapkit.js
+++ controllers/mapkit.js
@@ -4,14 +4,38 @@ require('dotenv').config()
 const fs = require('fs');
 
 const jwa = require('jwa');
-const request = require('request');
-const mkTokenGenerate = require('mapkit-token');
+var jwt = require('jsonwebtoken');
 
 
-const mkPrivKey = fs.readFileSync(__basedir + '/mapkit.p8');
+genToken = () => {
+	let privKey = fs.readFileSync(__basedir + '/mapkit.p8');
+	let mkKey =  process.env.MAPKEY
+	let teamID = process.env.TEAMID
+  let origin = process.env.WEBURL
+
+  const token = jwt.sign(
+    {
+      iat: Math.floor(Date.now() / 1000), // Now in seconds
+      iss: teamID,
+      origin: origin
+  	},
+    privKey,
+    {
+	    header: {
+        alg: "ES256",
+        typ: "JWT",
+        kid: mkKey
+	    },
+	    algorithm: "ES256",
+	    expiresIn: "30m"
+  	}
+  );
+
+  return token;
+}
 
 var exports = module.exports = {};
 
 exports.token = (req, res) => {
-  res.send(mkTokenGenerate(mkPrivKey, process.env.MAPKEY, process.env.TEAMID));
+  res.send(genToken());
 }
