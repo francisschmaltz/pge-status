4024f819f35f8ab684831465eefa53319cac1a58
Francis Schmaltz <schmaltz@cisco.com>
2026-09-19 02:01:09 -0700

    Update crypto dep

--- controllers/mapkit.js
+++ controllers/mapkit.js
@@ -1,8 +1,37 @@
 require("dotenv").config();
 
 const fs = require("fs");
+const crypto = require("crypto");
 
-const jwt = require("jsonwebtoken");
+const base64Url = (value) => Buffer.from(value).toString("base64")
+  .replace(/\+/g, "-")
+  .replace(/\//g, "_")
+  .replace(/=+$/, "");
+
+const readDerInteger = (signature, offset) => {
+  if (signature[offset] !== 0x02) throw new Error("Invalid ECDSA signature");
+  const length = signature[offset + 1];
+  const start = offset + 2;
+  const end = start + length;
+  let value = signature.slice(start, end);
+
+  while (value.length > 32 && value[0] === 0) value = value.slice(1);
+  if (value.length > 32) throw new Error("Invalid P-256 signature");
+
+  return {
+    value: Buffer.concat([Buffer.alloc(32 - value.length), value]),
+    next: end,
+  };
+};
+
+const derToJose = (signature) => {
+  if (signature[0] !== 0x30) throw new Error("Invalid ECDSA signature");
+  let offset = 2;
+  const r = readDerInteger(signature, offset);
+  offset = r.next;
+  const s = readDerInteger(signature, offset);
+  return Buffer.concat([r.value, s.value]);
+};
 
 const genToken = () => {
   const mkKey = process.env.MAPKEY;
@@ -26,26 +55,21 @@ const genToken = () => {
 
   const privKey = fs.readFileSync(__basedir + "/mapkit.p8");
 
-  const token = jwt.sign(
-    {
-      iat: Math.floor(Date.now() / 1000),
-      iss: teamID,
-      origin: origin,
-      scope: "mapkit_js",
-    },
-    privKey,
-    {
-      header: {
-        alg: "ES256",
-        typ: "JWT",
-        kid: mkKey,
-      },
-      algorithm: "ES256",
-      expiresIn: "30m",
-    }
-  );
+  const issuedAt = Math.floor(Date.now() / 1000);
+  const header = base64Url(JSON.stringify({ alg: "ES256", typ: "JWT", kid: mkKey }));
+  const payload = base64Url(JSON.stringify({
+    iss: teamID,
+    iat: issuedAt,
+    exp: issuedAt + 30 * 60,
+    origin,
+    scope: "mapkit_js",
+  }));
+  const signingInput = `${header}.${payload}`;
+  const signer = crypto.createSign("SHA256");
+  signer.update(signingInput);
+  signer.end();
 
-  return token;
+  return `${signingInput}.${base64Url(derToJose(signer.sign(privKey)))}`;
 };
 
 var exports = (module.exports = {});
